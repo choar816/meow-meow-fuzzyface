0b5f91c33fb06008871e9ab77708e3f7bf4e83cd
vince.joe <vince.joe@kakaocorp.com>
2022-05-29 14:35:12 +0900

    no more throttling!

--- src/utils/pause.js
+++ src/utils/pause.js
@@ -1,10 +1,10 @@
 import game from "../index";
 
 let global_scene_paused = false;
-let global_time_paused = (Date.now() - 10000);
+let global_time_paused = (Date.now() - 100);
 
 export default function global_pause(scene) {
-    if (Date.now() - global_time_paused > 2000 && game.scene.isActive(scene)) {
+    if (Date.now() - global_time_paused > 100 && game.scene.isActive(scene)) {
         game.scene.pause(scene);
         console.log('--------- global_pause called');
         global_time_paused = Date.now();
@@ -15,7 +15,7 @@ export default function global_pause(scene) {
 }
 
 document.addEventListener('keydown', function(event) {
-    if(event.key === 'Escape' && Date.now() - global_time_paused > 2000 && global_scene_paused) {
+    if(event.key === 'Escape' && Date.now() - global_time_paused > 100 && global_scene_paused) {
         game.scene.resume(global_scene_paused);
         console.log('+++++++++++ RESUME');
         game.scene.getScene(global_scene_paused).togglePauseScreen(false);
